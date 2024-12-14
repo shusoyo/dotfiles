45fdc7ea9f72dc4766affbdaa9b882b2b557e193
suspen <190865826+shusoyo@users.noreply.github.com>
2024-12-14 20:39:19 +0800

    update lazyvim
    1. delete chezmoi.lua, neorg.lua that I never used.
    2. change the colorscheme.lua, delete the flovr attribute to avoid
    error.
    3. update lazyvim

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,7 @@
 return {
   {
     "catppuccin/nvim",
-    opts = {
-      flavour = "mocha",
-      color_overrides = {
-        mocha = {
-          -- base = "#151516",
-        },
-      },
-    },
   },
-  -- { "loctvl842/monokai-pro.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
