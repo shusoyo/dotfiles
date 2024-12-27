53fa2c1305d705986eba09816e86529107bb0510
suspen <190865826+shusoyo@users.noreply.github.com>
2024-12-27 15:27:39 +0800

    Fix: kitty shell path, delete redundancy code

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,23 +1,16 @@
 function fish_prompt
-    set -l symbol '/> '
-
-    if test "$PWD" = "$HOME"
-        set symbol '> '
-    end
-
-    set -l color $fish_color_cwd
     #if fish_is_root_user
     #    set symbol ' # '
     #    set -q fish_color_cwd_root
     #    and set color $fish_color_cwd_root
     #end
 
-    # new line
+    set -l symbol '/> '
+
+    if test "$PWD" = "$HOME"
+        set symbol '> '
+    end
+
+    #set -l color $fish_color_cwd
     echo \n(prompt_pwd --full-length-dirs 2)$symbol
-    #
-    ##set_color $color
-    #echo -n (prompt_pwd --full-length-dirs 2)
-    ##set_color normal
-    #
-    #echo -n $symbol
 end
