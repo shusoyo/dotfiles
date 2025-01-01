ca25160e6770ac1422f9603fd07a51330283e8e5
suspen <190865826+shusoyo@users.noreply.github.com>
2025-01-01 09:02:58 +0800

    Dump, adjust nixpkgs of nix-darwin

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     sops-nix.url = "github:Mic92/sops-nix";
-    # sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, ... }: let
