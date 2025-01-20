2aa3a4362505f29dfcca620574e6f68c7f62b09f
suspen <190865826+shusoyo@users.noreply.github.com>
2025-01-20 23:49:02 +0800

    Feature: hws add blog action runner

--- hosts/hws/services.nix
+++ hosts/hws/services.nix
@@ -11,4 +11,14 @@
     extraLabels   = [ "nixos" ];
     extraPackages = [ pkgs.gitMinimal ];
   };
+
+  services.github-runners.blog = {
+    enable    = true;
+    name      = "blog";
+    url       = "https://github.com/shusoyo/shusoyo.github.io";
+    tokenFile = "${config.sops.secrets.gh-runner-token.path}";
+
+    extraLabels   = [ "nixos" ];
+    extraPackages = [ pkgs.gitMinimal ];
+  };
 }
