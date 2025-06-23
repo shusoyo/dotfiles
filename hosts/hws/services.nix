dab5a44fbe3cc1a4c413a74424a4c657756da5bf
suspen <190865826+shusoyo@users.noreply.github.com>
2025-06-23 11:27:44 +0800

    ~. Bump

--- hosts/hws/services.nix
+++ hosts/hws/services.nix
@@ -4,21 +4,21 @@
 
   services.github-runners.halfyear = {
     enable    = true;
-    name      = "halfyear-runner";
+    name      = "halfyear";
     url       = "https://github.com/shusoyo/halfyear";
     tokenFile = "${config.sops.secrets.gh-runner-token.path}";
 
     extraLabels   = [ "nixos" ];
     extraPackages = [ pkgs.gitMinimal ];
   };
-
-  services.github-runners.blog = {
-    enable    = true;
-    name      = "blog";
-    url       = "https://github.com/shusoyo/shusoyo.github.io";
-    tokenFile = "${config.sops.secrets.gh-runner-token.path}";
-
-    extraLabels   = [ "nixos" ];
-    extraPackages = [ pkgs.gitMinimal ];
-  };
+  #
+  # services.github-runners.blog = {
+  #   enable    = true;
+  #   name      = "blog";
+  #   url       = "https://github.com/shusoyo/shusoyo.github.io";
+  #   tokenFile = "${config.sops.secrets.gh-runner-token.path}";
+  #
+  #   extraLabels   = [ "nixos" ];
+  #   extraPackages = [ pkgs.gitMinimal ];
+  # };
 }
